6e790fe94f6ad0823aece3a65b91774530c6a9ab
Jack Davidson <jackmaverickavidson@gmail.com>
2023-01-11 11:28:49 -0600

    test stack function

--- test.cpp
+++ test.cpp
@@ -6,12 +6,19 @@
 #include "stack.h"
 #include "disc.h"
 
+using namespace std;
+
 void test_stack() {
+    std::cout << "Testing Stack()\n";
     Stack s = Stack();
-    char *str = (char *)malloc(6);
-    strcpy(str, "hello");
-    s.push(str);
-    std::cout << (char*)s.pop() << '\n';
+    string str = string("Hello test Data.");
+    std::cout << "\tinput  = " << str << '\n';
+    s.push(&str);
+    string popped = *(string *)s.pop();
+    std::cout << "\tpop() -> " << popped << '\n';
+    if (str.compare(popped) == 0) {
+        std::cout << "\tpassed" << std::endl;
+    }
 }
 
 int main() {
